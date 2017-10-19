31d952568d79c09836ba08b68f2c0d7563df7dff
MasashiMizuno <masajene8@gmail.com>
2017-10-19 11:33:00 +0900

    fix 0 padding

--- MMQreki/MMQreki.m
+++ MMQreki/MMQreki.m
@@ -26,7 +26,7 @@ static const NSInteger minDate = 1999;
 #pragma mark -------------------------------------------------------
 
 - (void)initializeTable {
-    
+
     self.replaceTable = [@[@[@611,@2350],
                        @[@468,@3222],
                        @[@316,@7317],
@@ -59,10 +59,10 @@ static const NSInteger minDate = 1999;
                        @[@356,@3366],
                        @[@572,@3475],
                        @[@442,@1450]] mutableCopy];
-    
+
     self.maxDays = [@[@31,@28,@31,@30,@31,@30,@31,@31,@30,@31,@30,@31] mutableCopy];
     self.oldTable = [@[@14] mutableCopy];
-    
+
     if (!self.dateFormatter) {
         self.dateFormatter = [[NSDateFormatter alloc] init];
         self.dateFormatter.dateFormat = @"yyyy/MM/dd";
@@ -74,11 +74,11 @@ static const NSInteger minDate = 1999;
 #pragma mark -------------------------------------------------------
 
 - (NSString *)getOldToNewCalender:(NSDate *)oldDate {
-    
+
     [self initializeTable];
-    
+
     NSString *oldDateString = [self.dateFormatter stringFromDate:oldDate];
-    
+
     NSNumber *newYear, *newMonth, *newDay, *tmp, *dofNewYear;
     NSNumber *oyy = @([[oldDateString substringToIndex:4] intValue]);
     NSNumber *omm = @([[oldDateString substringWithRange:NSMakeRange(5, 2)] intValue]);
@@ -117,30 +117,30 @@ static const NSInteger minDate = 1999;
     if ([newDay intValue] < 0) {
         return nil;
     }
-    return [NSString stringWithFormat:@"%@/%@/%@", newYear, newMonth, newDay];
+    return [NSString stringWithFormat:@"%@/%02d/%02d", oyy, [omm intValue], [odd intValue]];
 }
 
 - (NSString *)getNewToOldCalender:(NSDate *)newDate {
-    
+
     [self initializeTable];
-    
+
     NSString *newDateString = [self.dateFormatter stringFromDate:newDate];
-    
+
     NSNumber *oyy, *omm, *odd, *uruu, *DofNyy;
     NSNumber *nyy = @([[newDateString substringToIndex:4] intValue]);
     NSNumber *nmm = @([[newDateString substringWithRange:NSMakeRange(5, 2)] intValue]);
     NSNumber *ndd = @([[newDateString substringFromIndex:8] intValue]);
-    
+
     DofNyy = [self numberDays:nyy month:nmm day:ndd];
     oyy = nyy;
     [self tableExpand:oyy];
-    
+
     if ([DofNyy intValue] < [self.oldTable[0][0] intValue]) {
         oyy = @([oyy intValue] -1);
         DofNyy = @([DofNyy intValue] + 365 + [[self leapYear:oyy] intValue]);
         [self tableExpand:oyy];
     }
-    
+
     for (int i = 12; i >= 0; i--) {
         if (![self.oldTable[i][1] isEqualToNumber:@0]) {
             if ([self.oldTable[i][0] intValue] <= [DofNyy intValue]) {
@@ -150,7 +150,7 @@ static const NSInteger minDate = 1999;
             }
         }
     }
-    
+
     if ([omm intValue] < 0) {
         uruu = @1;
         omm = @([omm intValue] - [omm intValue]);
@@ -162,19 +162,19 @@ static const NSInteger minDate = 1999;
 }
 
 - (NSString *)getRokuyo:(NSDate *)newDate {
-    
+
     NSArray *rokuyo = @[@"大安",@"赤口",@"先勝",@"友引",@"先負",@"仏滅"];
-    
+
     NSString *oldCal = [self getNewToOldCalender:newDate];
     NSArray *sepCal = [oldCal componentsSeparatedByString:@"/"];
     NSInteger oldMonth = [sepCal[1] intValue];
     NSInteger oldDay = [sepCal[2] intValue];
-    
+
     NSInteger md = oldMonth + oldDay;
     float index = md % 6;
-    
+
     NSString *roku = rokuyo[(int)index];
-    
+
     return roku;
 }
 
@@ -193,7 +193,7 @@ static const NSInteger minDate = 1999;
 - (NSNumber *)numberDays:(NSNumber *)year month:(NSNumber *)month day:(NSNumber *)day {
 
     self.maxDays[1] = @(28 + [[self leapYear:year] intValue]);
-    
+
     NSNumber *days = day;
     for (int i = 1; i < [month intValue]; i++) {
         days = @([days intValue] + [self.maxDays[i-1] intValue]);
@@ -204,12 +204,12 @@ static const NSInteger minDate = 1999;
 - (void)tableExpand:(NSNumber *)year {
 
     NSNumber *ommax, *days, *uruu, *bit;
-    
+
     days = self.replaceTable[[year intValue] - minDate][0];
     bit = self.replaceTable[[year intValue] - minDate][1];
     uruu = @([days intValue] % 13);
     days = @(floor([days intValue] / 13 + 0.001));
-    
+
     self.oldTable[0] = @[days, @1];
     if ([uruu isEqualToNumber:@0]) {
         bit = @([bit intValue] * 2);
